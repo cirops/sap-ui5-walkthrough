2746bd054d969cc18d8e367612988604cd5fada1
cirops <cirops@gmail.com>
2025-07-09 17:26:47 -0300

    feat: chapter 9 (component configuration)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,36 +1,18 @@
 sap.ui.define(
-  [
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  (Controller, MessageToast, JSONModel, ResourceModel) => {
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
-      onInit() {
-        const oData = {
-          recipient: {
-            name: "World",
-          },
-        };
-        const oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
-
-        const i18nModel = new ResourceModel({
-          bundleName: "ui5.walkthrough.i18n.i18n",
-        });
-        this.getView().setModel(i18nModel, "i18n");
-      },
-
       onShowHello() {
+        // read msg from i18n model
         const oBundle = this.getView().getModel("i18n").getResourceBundle();
         const sRecipient = this.getView()
           .getModel()
           .getProperty("/recipient/name");
-        const sMsg = oBundle.getText("helloMsg", [sRecipient, 5]);
+        const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
+        // show message
         MessageToast.show(sMsg);
       },
     });
