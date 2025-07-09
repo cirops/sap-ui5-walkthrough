eb467067e22027757825a486164c4b1784ecca06
cirops <cirops@gmail.com>
2025-07-09 16:45:03 -0300

    feat: chapter 8 (translatable texts)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  (Controller, MessageToast, JSONModel) => {
+  (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
@@ -16,10 +17,21 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+
+        const i18nModel = new ResourceModel({
+          bundleName: "ui5.walkthrough.i18n.i18n",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
 
       onShowHello() {
-        MessageToast.show("Hello World");
+        const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        const sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        const sMsg = oBundle.getText("helloMsg", [sRecipient, 5]);
+
+        MessageToast.show(sMsg);
       },
     });
   }
